521f7f93c9391e4cc0355661b58335066042ff0d
benson <benson@jilcreation.onmicrosoft.com>
2019-05-03 15:43:33 +0800

    asdasda

--- src/__test__/PopularPage.test.js
+++ src/__test__/PopularPage.test.js
@@ -3,14 +3,6 @@ import renderer from "react-test-renderer"; // ES6
 import PopularPage from "../views/PopularPage";
 import TrendingPage from "../views/TrendingPage";
 
-// describe("Popular Page", () => {
-//   it("Show a list of popular movies", () => {
-//     const component = renderer.create(<PopularPage />);
-//     const instance = component.getInstance();
-//     console.log(instance);
-//   });
-// });
-
 describe("Trending Page", () => {
   it("Shows trending movies list", () => {
     const component = renderer.create(<TrendingPage />);
